7a509fe39ec4d3ee2fbf8ceb5356eed76805e062
Manuela França <manuelaalves26@gmail.com>
2023-04-25 16:00:21 -0300

    feat: implementa teste da função encrypt_message.

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("mensagem", "secreta")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(77, 8)
+
+    result_message = encrypt_message("secreta", 3)
+    mock_message = "ces_ater"
+
+    result_message_big = encrypt_message("secreta", 7)
+    mock_message_big = "aterces"
+
+    assert mock_message == result_message
+    assert mock_message_big == result_message_big
