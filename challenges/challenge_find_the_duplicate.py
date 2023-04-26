495f0db1804b875d2c06dec3bdc632bd33c77109
Manuela França <manuelaalves26@gmail.com>
2023-04-26 12:07:15 -0300

    feat: implementa algoritmo de busca na função find_duplicate.

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+
+    if len(nums) <= 1:
+        return False
+
+    for i in range(1, len(nums)):
+        if not isinstance(nums[i], int) or nums[i] < 0:
+            return False
+        if nums[i] == nums[i - 1]:
+            return nums[i]
+    return False
